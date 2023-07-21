f67961b48157cf19f8fbdc574556d47bf605838c
Blu.Fi Tech, Inc <blufitech@gmail.com>
2023-07-21 04:02:30 -0400

    Update blocked.ts

--- pages/api/blocked.ts
+++ pages/api/blocked.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
     error:
-      "We love that you want to keep trying us out! Feel free to clone this repository in Vercel and continue using it yourself.",
+      "⭐We appreciate your time and wish you the best of luck!⭐",
   });
   return res.end();
 }
